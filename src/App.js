6836277b55febbd32ccdf24f8953750c5685e8db
rocuant <roberto.rocuantv@gmail.com>
2023-06-03 13:31:55 -0400

    app1 ready league of legends examples

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import { Review } from './components/review';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <h1>League of legends</h1>
+      <div class='main-container'>
+        <Review
+          name='Zed'
+          skin='Project'
+          image='zed.jpeg'
+          text="Ruthless and merciless, Zed is the leader of the Order of Shadows, an organization he created with the intention of militarizing Ionia's martial and magical arts traditions to repel Noxian invaders. During the war, desperation drove him to reveal the secret ways of the shadows, a malevolent spiritual magic as powerful as it was corrupt and dangerous."
+        />
+        <Review
+          name='Anivia'
+          skin='Festival Queen'
+          image='anivia.jpg'
+          text="Anivia is a benevolent winged spirit who endures endless cycles of life, death and resurrection to protect Fréljord. Born from the union of cruel ice and piercing wind, she is a demigoddess who uses these elemental powers to thwart anyone who dares to disturb her homeland."
+        />
+        <Review
+          name='Twisted Fate'
+          skin='Default'
+          image='twistedfate.jpg'
+          text="Twisted Fate is a famed card expert and con man. He has gambled and used his charm on much of the known world, earning him the hatred and admiration of rich and foolish alike. Rara vez se toma las cosas en serio, despierta cada día con una burlesca sonrisa y un descuidado aire de fanfarrón."
+        />
+      </div>
     </div>
   );
 }
--- src/components/review.js
+++ src/components/review.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import '../static/review.css';
 
-function Review() {
+export function Review(props) {
   return (
     <div className='review-container'>
-      <img className='review-image' src={require('../imgs/zed.jpeg')} alt='Zed' />
+      <img className='review-image' src={require(`../imgs/${props.image}`)} alt='Zed' width="200" height="200" />
+      <div className='review-text-container'>
+        <p className='review-name'><strong>{props.name}</strong></p>
+        <p className='review-skin'>{props.skin}</p>
+        <p className='review-text'>{props.text}</p>
+      </div>
     </div>
   );
 }
