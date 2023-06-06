99a0fbc1dbcf7612b14b53f95a6be5cc3e1239ab
rocuant <roberto.rocuantv@gmail.com>
2023-06-06 16:48:57 -0400

    app1 some fix

--- src/components/review.js
+++ src/components/review.js
@@ -4,7 +4,7 @@ import '../static/review.css';
 export function Review(props) {
   return (
     <div className='review-container'>
-      <img className='review-image' src={require(`../imgs/${props.image}`)} alt='Zed' width="200" height="200" />
+      <img className='review-image' src={require(`../imgs/${props.image}`)} alt='Zed' />
       <div className='review-text-container'>
         <p className='review-name'><strong>{props.name}</strong></p>
         <p className='review-skin'>{props.skin}</p>
